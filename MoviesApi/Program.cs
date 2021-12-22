59c6a0f21a1f98826197f527eaafe975c56aa885
Muhammad ElHelaly <mae@leanpeach.com>
2021-12-22 14:35:24 +0200

    Allow adding JWT key to Swagger

--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -7,7 +7,54 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo 
+    {
+        Version = "v1",
+        Title = "MoviesApi",
+        Description = "My first api",
+        TermsOfService = new Uri("https://www.google.com"),
+        Contact = new OpenApiContact 
+        {
+            Name = "DevCreed",
+            Email = "test@domain.com",
+            Url = new Uri("https://www.google.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "My license",
+            Url = new Uri("https://www.google.com")
+        }
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme 
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement 
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
